a9273761166ef3e55198f58bd06ded3108b66567
Somacros <marcoseduardo2604@gmail.com>
2020-03-17 22:24:08 -0700

    Updates

---
+++ precache-manifest.29a24430ddfa1436bd6fdc04a2e81c27.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce96ead53670622270c06d496649409a",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "978d3e6abce16fc74e6c",
+    "url": "/robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "7d88619aa42a047ebf56",
+    "url": "/robofriends/static/css/main.4871f943.chunk.css"
+  },
+  {
+    "revision": "978d3e6abce16fc74e6c",
+    "url": "/robofriends/static/js/2.faf57d51.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/robofriends/static/js/2.faf57d51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d88619aa42a047ebf56",
+    "url": "/robofriends/static/js/main.669e4fed.chunk.js"
+  },
+  {
+    "revision": "6a5eb8d065c05feaff0d",
+    "url": "/robofriends/static/js/runtime-main.5530ba65.js"
+  },
+  {
+    "revision": "fb6aaf7b4d384b517c1ae2f06d837c40",
+    "url": "/robofriends/static/media/Risque-Regular.fb6aaf7b.ttf"
+  }
+]);
